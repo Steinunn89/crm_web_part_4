3f7ce219aaaec525ec4c1d81725b678c602caf51
Steinunn <steinunn.olason@gmail.com>
2014-10-31 13:58:08 -0400

    Final clean up

--- Part 4/crm.rb
+++ Part 4/crm.rb
@@ -71,10 +71,8 @@ put "/contacts/:id" do
   @contact = Contact.get(params[:id].to_i)
   if @contact
       @contact.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], note: params[:note])
-      
-    redirect to("/contacts")
 
-    #do I need to use .replace??
+    redirect to("/contacts")
   else
     raise Sinatra::NotFound
   end
